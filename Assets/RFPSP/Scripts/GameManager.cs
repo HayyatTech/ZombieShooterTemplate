5ab2ff943b380098f6f6fc13ab4cf1eb576acdb5
Salman Hayat <salmanhayat16@gmail.com>
2021-06-06 01:47:52 +0500

    initial commit

---
+++ Assets/RFPSP/Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerWeapons playerWeapons;
+    public GameObject WinPanel;
+    public GameObject PlayerControls;
+    public Text Levelno,Objective,rewardText; 
+    private int Score;
+    public static int Kills;
+    public static GameManager instance;
+  
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        Kills = 0;
+        Objective.text = "Zombies " + Kills + "/" + CurrentLevel.currentLevel.SpawnedZombies;
+        Levelno.text ="Level "+ CurrentLevel.currentLevel.levelNo;
+        Score = PlayerPrefs.GetInt("score");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void CheckLevelProgress()
+    {
+        if (Kills == CurrentLevel.currentLevel.SpawnedZombies)
+        {
+           OnCompleteLevel();
+        }
+        instance.Objective.text = "Zombies " + Kills + "/" + CurrentLevel.currentLevel.SpawnedZombies;
+    }
+
+    public static void OnCompleteLevel()
+    {
+        instance.PlayerControls.SetActive(false);
+        instance.WinPanel.SetActive(true);
+        instance.rewardText.text = "Reward " + CurrentLevel.currentLevel.Reward;
+        instance.Score += CurrentLevel.currentLevel.Reward;
+        PlayerPrefs.SetInt("score",instance.Score);
+    }
+
+    public void Home()
+    {
+        Destroy(CurrentLevel.currentLevel);
+        SceneManager.LoadScene(0);
+    }
+    public void NextLevel()
+    {
+        CurrentLevel.currentLevel.OnclickLevelButton(CurrentLevel.currentLevel.CurrentLevelNo + 1);
+    }
+
+    public void AddAmmo()
+    {
+        for(int i = 0; i < playerWeapons.weaponOrder.Length; i++)
+        {
+            if (playerWeapons.weaponOrder[i].gameObject.activeSelf)
+            {
+                playerWeapons.weaponOrder[i].GetComponent<WeaponBehavior>().ammo += 30;
+            }
+        }
+    }
+}
---
+++ Assets/UImanager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UImanager : MonoBehaviour
+{
+    public Text Score;
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("score") == 0)
+        {
+            PlayerPrefs.SetInt("score", 500);
+        }
+        Score.text = PlayerPrefs.GetInt("score").ToString();
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
